c2c46ca34b6c5cf922bf89c3a2b81b8de20ee09f
amirali <230751168+amiralihadiani@users.noreply.github.com>
2026-09-21 10:17:16 +0330

    AnimationPlayer

---
+++ Bomberman/Assets/Scripts/Player/AnimationPlayer.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class AnimationPlayer : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField] private Sprite idleSprite;
+    [SerializeField] private Sprite[] animationSprites;
+
+    [SerializeField] private float animationTime = 0.25f;
+
+    private int animationFrame;
+
+    public bool loop = true;
+    public bool idle = true;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        spriteRenderer.enabled = true;
+        animationFrame = 0;
+    }
+
+    private void OnDisable()
+    {
+        spriteRenderer.enabled = false;
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(
+            nameof(NextFrame),
+            animationTime,
+            animationTime
+        );
+    }
+
+    private void NextFrame()
+    {
+        if (idle)
+        {
+            spriteRenderer.sprite = idleSprite;
+            return;
+        }
+
+        animationFrame++;
+
+        if (loop && animationFrame >= animationSprites.Length)
+        {
+            animationFrame = 0;
+        }
+
+        if (animationFrame >= 0 &&
+            animationFrame < animationSprites.Length)
+        {
+            spriteRenderer.sprite = animationSprites[animationFrame];
+        }
+    }
+}
--- Bomberman/Assets/Scripts/Player/PlayerMovement.cs
+++ Bomberman/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,10 +7,18 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 direction = Vector2.down;
 
     [SerializeField] private float speed = 5f;
+    
+    [SerializeField] AnimationPlayer spriteRendererUp;
+    [SerializeField] AnimationPlayer spriteRendererDown;
+    [SerializeField] AnimationPlayer spriteRendererLeft;
+    [SerializeField] AnimationPlayer spriteRendererRight;
+    private AnimationPlayer _activeSpriteRenderer;
 
     private void Awake()
     {
         rigidbodyPlayer = GetComponent<Rigidbody2D>();
+        _activeSpriteRenderer = spriteRendererDown;
+
     }
 
     private void Update()
@@ -21,7 +29,7 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         Vector2 position = rigidbodyPlayer.position;
-        Vector2 translation = direction * speed * Time.fixedDeltaTime;
+        Vector2 translation = direction * (speed * Time.fixedDeltaTime);
 
         rigidbodyPlayer.MovePosition(position + translation);
     }
@@ -30,28 +38,35 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Keyboard.current.aKey.isPressed)
         {
-            SetDirection(Vector2.left);
+            SetDirection(Vector2.left,spriteRendererLeft);
         }
         else if (Keyboard.current.dKey.isPressed)
         {
-            SetDirection(Vector2.right);
+            SetDirection(Vector2.right,spriteRendererRight);
         }
         else if (Keyboard.current.wKey.isPressed)
         {
-            SetDirection(Vector2.up);
+            SetDirection(Vector2.up,spriteRendererUp);
         }
         else if (Keyboard.current.sKey.isPressed)
         {
-            SetDirection(Vector2.down);
+            SetDirection(Vector2.down,spriteRendererDown);
         }
         else
         {
-            SetDirection(Vector2.zero);
+            SetDirection(Vector2.zero,_activeSpriteRenderer);
         }
     } 
 
-    private void SetDirection(Vector2 newDirection)
+    private void SetDirection(Vector2 newDirection,AnimationPlayer spriteRenderer)
     {
         direction = newDirection;
+        spriteRendererUp.enabled = spriteRenderer == spriteRendererUp;
+        spriteRendererDown.enabled = spriteRenderer == spriteRendererDown;
+        spriteRendererLeft.enabled = spriteRenderer == spriteRendererLeft;
+        spriteRendererRight.enabled = spriteRenderer == spriteRendererRight;
+
+        _activeSpriteRenderer = spriteRenderer;
+        _activeSpriteRenderer.idle = direction == Vector2.zero;
     }
 }
